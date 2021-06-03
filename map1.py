9c28dca3aa8ec764a1d95832f3c76af1ad22d472
F1shift <formyfarrari@gmail.com>
2021-06-04 02:02:19 +0900

    20210604

--- map1.py
+++ map1.py
@@ -3,11 +3,17 @@ import pandas
 from random import uniform
 
 datas = pandas.read_csv("Volcanoes.txt")
-center = [datas["LAT"][0], datas["LON"][0]]
+center = [41.795369, -112.692524]
 map = folium.Map(location=center, zoom_start=6, tiles="Stamen Terrain")
+popup_template = """<h4>Volcano Name:</h4>
+<a href="https://www.google.com/search?q=%22{name}%22" target="_blank">{name}</a><br>
+{elev}m
+"""
 feature_group = folium.FeatureGroup(name="My Map")
-for name, lat, lon in zip(datas["NAME"], datas["LAT"], datas["LON"]):
+for name, lat, lon, elev in zip(datas["NAME"], datas["LAT"], datas["LON"], datas["ELEV"]):
     point = [ lat, lon ]
-    feature_group.add_child(folium.Marker(location=point, popup=name, icon=folium.Icon(color="green")))
+    popup = folium.Popup(folium.IFrame(html=popup_template.format(name=name, elev=elev), width=200, height=100))
+    feature_group.add_child(folium.Marker(location=point, popup=popup, icon=folium.Icon(color="green")))
+
 map.add_child(feature_group)
 map.save("map1.html")
